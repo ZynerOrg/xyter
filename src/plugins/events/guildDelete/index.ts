574abd3930e778a59662057f3a8b2ac566609887
Vermium Sifell <vermium@zyner.org>
2022-10-19 09:37:48 +0200

    ♻️ dropGuild is now done via Prisma

--- src/plugins/events/guildDelete/index.ts
+++ src/plugins/events/guildDelete/index.ts
@@ -4,6 +4,7 @@ import { Guild } from "discord.js";
 import dropGuild from "../../../helpers/deleteGuildData";
 import updatePresence from "../../../helpers/updatePresence";
 import { IEventOptions } from "../../../interfaces/EventOptions";
+import prisma from "../../../prisma";
 
 export const options: IEventOptions = {
   type: "on",
@@ -12,6 +13,23 @@ export const options: IEventOptions = {
 export const execute = async (guild: Guild) => {
   const { client } = guild;
 
-  await dropGuild(guild);
   await updatePresence(client);
+  await dropGuild(guild);
+
+  // Delete guildMember objects
+  const deleteGuildMembers = prisma.guildMember.deleteMany({
+    where: {
+      guildId: guild.id,
+    },
+  });
+
+  // Delete guild object
+  const deleteGuild = prisma.guild.deleteMany({
+    where: {
+      id: guild.id,
+    },
+  });
+
+  // The transaction runs synchronously so deleteUsers must run last.
+  await prisma.$transaction([deleteGuildMembers, deleteGuild]);
 };
