10283f911bcf36065c2944b912e01c49ff71aaa8
Vermium Sifell <vermium@zyner.org>
2022-10-19 09:54:05 +0200

    ♻️ Migrate guildCreate to Prisma

--- src/plugins/events/guildCreate/index.ts
+++ src/plugins/events/guildCreate/index.ts
@@ -1,7 +1,8 @@
 import { Guild } from "discord.js";
-import fetchGuild from "../../../helpers/guildData";
 import updatePresence from "../../../helpers/updatePresence";
 import { IEventOptions } from "../../../interfaces/EventOptions";
+import logger from "../../../middlewares/logger";
+import prisma from "../../../prisma";
 
 export const options: IEventOptions = {
   type: "on",
@@ -10,6 +11,40 @@ export const options: IEventOptions = {
 export const execute = async (guild: Guild) => {
   const { client } = guild;
 
-  await fetchGuild(guild);
   await updatePresence(client);
+
+  // Create guildMember object
+  const createGuildMember = await prisma.guildMember.upsert({
+    where: {
+      userId_guildId: {
+        userId: guild.ownerId,
+        guildId: guild.id,
+      },
+    },
+    update: {},
+    create: {
+      user: {
+        connectOrCreate: {
+          create: {
+            id: guild.ownerId,
+          },
+          where: {
+            id: guild.ownerId,
+          },
+        },
+      },
+      guild: {
+        connectOrCreate: {
+          create: {
+            id: guild.id,
+          },
+          where: {
+            id: guild.id,
+          },
+        },
+      },
+    },
+  });
+
+  logger.silly(createGuildMember);
 };
