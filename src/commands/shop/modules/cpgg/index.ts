f412ad6fae1212a4ac5b13ce570108cb31b0cc48
Vermium Sifell <vermium@zyner.org>
2022-10-20 12:48:40 +0200

    ♻️ Fixed /shop cpgg to Prisma

--- src/commands/shop/modules/cpgg/index.ts
+++ src/commands/shop/modules/cpgg/index.ts
@@ -1,6 +1,18 @@
 import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
-import { ChatInputCommandInteraction } from "discord.js";
-// import fetchUser from "../../../../helpers/userData";
+import axios from "axios";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  Message,
+} from "discord.js";
+import { v4 as uuidv4 } from "uuid";
+import prisma from "../../../../handlers/database";
+import encryption from "../../../../helpers/encryption";
+import getEmbedData from "../../../../helpers/getEmbedData";
+import logger from "../../../../middlewares/logger";
 
 export default {
   metadata: { guildOnly: true, ephemeral: true },
@@ -17,154 +29,166 @@ export default {
       );
   },
   execute: async (interaction: ChatInputCommandInteraction) => {
-    // const { errorColor, successColor, footerText, footerIcon } =
-    //   await getEmbedConfig(interaction.guild);
-    // const { options, guild, user, client } = interaction;
-    // const optionAmount = options?.getInteger("amount");
-    // if (optionAmount === null) {
-    //   logger?.silly(`Amount is null.`);
-    //   const interactionEmbed = new EmbedBuilder()
-    //     .setTitle("[:dollar:] Gift")
-    //     .setDescription("We could not read your requested amount.")
-    //     .setTimestamp()
-    //     .setColor(errorColor)
-    //     .setFooter({ text: footerText, iconURL: footerIcon });
-    //   return interaction?.editReply({
-    //     embeds: [interactionEmbed],
-    //   });
-    // }
-    // if (guild === null) {
-    //   return logger?.silly(`Guild is null`);
-    // }
-    // const userDB = await fetchUser(user, guild);
-    // if (userDB === null) {
-    //   return logger?.silly(`User is null`);
-    // }
-    // const dmUser = client?.users?.cache?.get(user?.id);
-    // if ((optionAmount || userDB?.credits) < 100) {
-    //   logger?.silly(`Amount or user credits is below 100.`);
-    //   const interactionEmbed = new EmbedBuilder()
-    //     .setTitle("[:shopping_cart:] CPGG")
-    //     .setDescription("You **can't** withdraw for __CPGG__ below **100**.")
-    //     .setTimestamp()
-    //     .addFields({
-    //       name: "Your balance",
-    //       value: `${pluralize(userDB?.credits, "credit")}`,
-    //     })
-    //     .setColor(errorColor)
-    //     .setFooter({ text: footerText, iconURL: footerIcon });
-    //   return interaction?.editReply({
-    //     embeds: [interactionEmbed],
-    //   });
-    // }
-    // if ((optionAmount || userDB?.credits) > 1000000) {
-    //   logger?.silly(`Amount or user credits is above 1.000.000.`);
-    //   const interactionEmbed = new EmbedBuilder()
-    //     .setTitle("[:shopping_cart:] CPGG")
-    //     .setDescription(
-    //       "You **can't** withdraw for __CPGG__ above **1.000.000**."
-    //     )
-    //     .setTimestamp()
-    //     .addFields({
-    //       name: "Your balance",
-    //       value: `${pluralize(userDB?.credits, "credit")}`,
-    //     })
-    //     .setColor(errorColor)
-    //     .setFooter({ text: footerText, iconURL: footerIcon });
-    //   return interaction?.editReply({
-    //     embeds: [interactionEmbed],
-    //   });
-    // }
-    // if (userDB?.credits < optionAmount) {
-    //   logger?.silly(`User credits is below amount.`);
-    //   const interactionEmbed = new EmbedBuilder()
-    //     .setTitle("[:shopping_cart:] CPGG")
-    //     .setDescription("You have **insufficient** credits.")
-    //     .setTimestamp()
-    //     .addFields({
-    //       name: "Your balance",
-    //       value: `${pluralize(userDB?.credits, "credit")}`,
-    //     })
-    //     .setColor(errorColor)
-    //     .setFooter({ text: footerText, iconURL: footerIcon });
-    //   return interaction?.editReply({
-    //     embeds: [interactionEmbed],
-    //   });
-    // }
-    // const code = uuidv4();
-    // const apiCredentials = await apiSchema?.findOne({
-    //   guildId: guild?.id,
-    // });
-    // if (!apiCredentials) return;
-    // const url = encryption.decrypt(apiCredentials?.url);
-    // const api = axios?.create({
-    //   baseURL: `${url}/api/`,
-    //   headers: {
-    //     Authorization: `Bearer ${encryption.decrypt(apiCredentials.token)}`,
-    //   },
-    // });
-    // const shopUrl = `${url}/store`;
-    // const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
-    //   new ButtonBuilder()
-    //     .setLabel("Redeem it here")
-    //     .setStyle(ButtonStyle.Link)
-    //     .setEmoji("🏦")
-    //     .setURL(`${shopUrl}?voucher=${code}`)
-    // );
-    // await api
-    //   ?.post("vouchers", {
-    //     uses: 1,
-    //     code,
-    //     credits: optionAmount || userDB?.credits,
-    //     memo: `${interaction?.createdTimestamp} - ${interaction?.user?.id}`,
-    //   })
-    //   ?.then(async () => {
-    //     logger?.silly(`Successfully created voucher.`);
-    //     userDB.credits -= optionAmount || userDB?.credits;
-    //     await userDB
-    //       ?.save()
-    //       ?.then(async () => {
-    //         logger?.silly(`Successfully saved new credits.`);
-    //         if (!interaction.guild) throw new Error("Guild is undefined");
-    //         const dmEmbed = new EmbedBuilder()
-    //           .setTitle("[:shopping_cart:] CPGG")
-    //           .setDescription(
-    //             `This voucher comes from **${interaction.guild.name}**.`
-    //           )
-    //           .setTimestamp()
-    //           .addFields({
-    //             name: "💶 Credits",
-    //             value: `${optionAmount || userDB?.credits}`,
-    //             inline: true,
-    //           })
-    //           .setColor(successColor)
-    //           .setFooter({ text: footerText, iconURL: footerIcon });
-    //         await dmUser
-    //           ?.send({
-    //             embeds: [dmEmbed],
-    //             components: [buttons],
-    //           })
-    //           .then(async (msg: Message) => {
-    //             const interactionEmbed = new EmbedBuilder()
-    //               .setTitle("[:shopping_cart:] CPGG")
-    //               .setDescription(
-    //                 `I have sent you the code in [DM](${msg.url})!`
-    //               )
-    //               .setTimestamp()
-    //               .setColor(successColor)
-    //               .setFooter({ text: footerText, iconURL: footerIcon });
-    //             await interaction?.editReply({
-    //               embeds: [interactionEmbed],
-    //             });
-    //             return;
-    //           });
-    //       })
-    //       .catch(() => {
-    //         throw new Error("Failed to update credits for user.");
-    //       });
-    //   })
-    //   .catch(() => {
-    //     throw new Error("Failed generating an voucher.");
-    //   });
+    const { errorColor, successColor, footerText, footerIcon } =
+      await getEmbedData(interaction.guild);
+    const { options, guild, user, client } = interaction;
+    const optionAmount = options?.getInteger("amount");
+    if (optionAmount === null) {
+      logger?.silly(`Amount is null.`);
+      const interactionEmbed = new EmbedBuilder()
+        .setTitle("[:dollar:] Gift")
+        .setDescription("We could not read your requested amount.")
+        .setTimestamp()
+        .setColor(errorColor)
+        .setFooter({ text: footerText, iconURL: footerIcon });
+      return interaction?.editReply({
+        embeds: [interactionEmbed],
+      });
+    }
+    if (!guild) throw new Error("Guild not found");
+
+    const createGuildMember = await prisma.guildMember.upsert({
+      where: {
+        userId_guildId: {
+          userId: user.id,
+          guildId: guild.id,
+        },
+      },
+      update: {},
+      create: {
+        user: {
+          connectOrCreate: {
+            create: {
+              id: user.id,
+            },
+            where: {
+              id: user.id,
+            },
+          },
+        },
+        guild: {
+          connectOrCreate: {
+            create: {
+              id: guild.id,
+            },
+            where: {
+              id: guild.id,
+            },
+          },
+        },
+      },
+      include: {
+        user: true,
+        guild: true,
+      },
+    });
+
+    logger.silly(createGuildMember);
+
+    const dmUser = client?.users?.cache?.get(user?.id);
+
+    if ((optionAmount || createGuildMember.creditsEarned) < 100)
+      throw new Error("You can't withdraw to CPGG below 100 credits.");
+
+    if ((optionAmount || createGuildMember.creditsEarned) > 1000000)
+      throw new Error("Amount or user credits is above 1.000.000.");
+
+    if (createGuildMember.creditsEarned < optionAmount)
+      throw new Error("You can't withdraw more than you have on your account.");
+
+    if (
+      !createGuildMember.guild.apiCpggUrlIv ||
+      !createGuildMember.guild.apiCpggUrlContent
+    )
+      throw new Error("No API url available");
+
+    if (
+      !createGuildMember.guild.apiCpggTokenIv ||
+      !createGuildMember.guild.apiCpggTokenContent
+    )
+      throw new Error("No API token available");
+
+    const code = uuidv4();
+    const url = encryption.decrypt({
+      iv: createGuildMember.guild.apiCpggUrlIv,
+      content: createGuildMember.guild.apiCpggUrlContent,
+    });
+    const api = axios?.create({
+      baseURL: `${url}/api/`,
+      headers: {
+        Authorization: `Bearer ${encryption.decrypt({
+          iv: createGuildMember.guild.apiCpggTokenIv,
+          content: createGuildMember.guild.apiCpggTokenContent,
+        })}`,
+      },
+    });
+    const shopUrl = `${url}/store`;
+    const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setLabel("Redeem it here")
+        .setStyle(ButtonStyle.Link)
+        .setEmoji("🏦")
+        .setURL(`${shopUrl}?voucher=${code}`)
+    );
+    await api
+      ?.post("vouchers", {
+        uses: 1,
+        code,
+        credits: optionAmount || createGuildMember.creditsEarned,
+        memo: `${interaction?.createdTimestamp} - ${interaction?.user?.id}`,
+      })
+      ?.then(async () => {
+        logger?.silly(`Successfully created voucher.`);
+        createGuildMember.creditsEarned -=
+          optionAmount || createGuildMember.creditsEarned;
+
+        const updateGuildMember = await prisma.guildMember.update({
+          where: {
+            userId_guildId: {
+              userId: user.id,
+              guildId: guild.id,
+            },
+          },
+          data: {
+            creditsEarned: {
+              decrement: optionAmount || createGuildMember.creditsEarned,
+            },
+          },
+        });
+
+        logger.silly(updateGuildMember);
+
+        if (!interaction.guild) throw new Error("Guild is undefined");
+        const dmEmbed = new EmbedBuilder()
+          .setTitle("[:shopping_cart:] CPGG")
+          .setDescription(
+            `This voucher comes from **${interaction.guild.name}**.`
+          )
+          .setTimestamp()
+          .addFields({
+            name: "💶 Credits",
+            value: `${optionAmount || createGuildMember.creditsEarned}`,
+            inline: true,
+          })
+          .setColor(successColor)
+          .setFooter({ text: footerText, iconURL: footerIcon });
+        await dmUser
+          ?.send({
+            embeds: [dmEmbed],
+            components: [buttons],
+          })
+          .then(async (msg: Message) => {
+            const interactionEmbed = new EmbedBuilder()
+              .setTitle("[:shopping_cart:] CPGG")
+              .setDescription(`I have sent you the code in [DM](${msg.url})!`)
+              .setTimestamp()
+              .setColor(successColor)
+              .setFooter({ text: footerText, iconURL: footerIcon });
+            await interaction?.editReply({
+              embeds: [interactionEmbed],
+            });
+            return;
+          });
+      });
   },
 };
