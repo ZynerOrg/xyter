ea8e3bb1dced16f3f477464b9ba6d839f49bd4b3
Vermium Sifell <vermium@zyner.org>
2022-10-20 10:13:32 +0200

    🗃️ added support for cpgg in Prisma

---
+++ prisma/migrations/20221020081238_cpgg_api/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Guild" ADD COLUMN "apiCpggTokenContent" TEXT;
+ALTER TABLE "Guild" ADD COLUMN "apiCpggTokenIv" TEXT;
+ALTER TABLE "Guild" ADD COLUMN "apiCpggUrlContent" TEXT;
+ALTER TABLE "Guild" ADD COLUMN "apiCpggUrlIv" TEXT;
