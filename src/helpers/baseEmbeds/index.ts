3c49580831b7627191d8211070fb0256e39e239f
Vermium Sifell <vermium@zyner.org>
2022-10-22 16:03:37 +0200

    💡 Add comment for baseEmbeds

--- src/helpers/baseEmbeds/index.ts
+++ src/helpers/baseEmbeds/index.ts
@@ -1,6 +1,7 @@
 import { EmbedBuilder, Guild } from "discord.js";
 import getEmbedData from "../getEmbedData";
 
+// Construct a base embed for success messages
 export const success = async (guild: Guild | null, title: string) => {
   const { successColor, footerText, footerIcon } = await getEmbedData(guild);
 
@@ -11,6 +12,7 @@ export const success = async (guild: Guild | null, title: string) => {
     .setFooter({ text: footerText, iconURL: footerIcon });
 };
 
+// Construct a base embed for wait messages
 export const wait = async (guild: Guild | null, title: string) => {
   const { waitColor, footerText, footerIcon } = await getEmbedData(guild);
 
@@ -21,6 +23,7 @@ export const wait = async (guild: Guild | null, title: string) => {
     .setFooter({ text: footerText, iconURL: footerIcon });
 };
 
+// Construct a base embed for error messages
 export const error = async (guild: Guild | null, title: string) => {
   const { errorColor, footerText, footerIcon } = await getEmbedData(guild);
 
