d79d38c0533d2709206929115d5899fa587111fa
Vermium Sifell <vermium@zyner.org>
2022-12-04 17:10:27 +0100

    chore(ux): :children_crossing: Moved /reputation give => /reputation repute and improved UX
    /reputation repute makes more sense than /reputation give and I also updated messages to be more user engaging
    
    Moved /reputation give to /reputation repute

--- src/commands/reputation/index.ts
+++ src/commands/reputation/index.ts
@@ -3,22 +3,22 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 // Modules
 import moduleCheck from "./modules/check";
-import moduleGive from "./modules/give";
+import moduleRepute from "./modules/repute";
 
 // Function
 export const builder = new SlashCommandBuilder()
   .setName("reputation")
-  .setDescription("Manage reputation.")
+  .setDescription("See and repute users to show other how trustworthy they are")
   .setDMPermission(false)
 
   // Modules
-  .addSubcommand(moduleGive.builder)
+  .addSubcommand(moduleRepute.builder)
   .addSubcommand(moduleCheck.builder);
 
 // Execute function
 export const execute = async (interaction: ChatInputCommandInteraction) => {
-  if (interaction.options.getSubcommand() === "give") {
-    await moduleGive.execute(interaction);
+  if (interaction.options.getSubcommand() === "repute") {
+    await moduleRepute.execute(interaction);
     return;
   }
   if (interaction.options.getSubcommand() === "check") {
