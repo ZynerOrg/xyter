40b640f88e69d806ba047a205a349be23a76d5ac
Vermium Sifell <vermium@zyner.org>
2022-12-15 10:25:52 +0100

    chore: :fire: Remove dist folder
    I removed dist folder again since I accidently build and pushed it in the CI
    
    No dist folder in the repo

--- dist/helpers/checkPermission/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (interaction, permission) => {
-    if (!interaction.memberPermissions)
-        throw new Error("Could not check user for permissions");
-    if (!interaction.memberPermissions.has(permission))
-        throw new Error("Permission denied");
-};
