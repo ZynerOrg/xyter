f0121206b59160eed0ca01a734a96d4a166fb3e6
Vermium Sifell <vermium@zyner.org>
2022-12-15 12:36:24 +0100

    style(ux): :children_crossing: update audit embed
    Updated audit embeds for guildMemberAdd and guildMemberRemove event

--- src/events/guildMemberRemove/audits.ts
+++ src/events/guildMemberRemove/audits.ts
@@ -1,21 +1,38 @@
+import { formatDistanceToNow } from "date-fns";
 import { EmbedBuilder, GuildMember } from "discord.js";
 import auditLogger from "../../helpers/auditLogger";
+import capitalizeFirstLetter from "../../helpers/capitalizeFirstLetter";
 
 export default {
   execute: async (member: GuildMember) => {
     const { guild } = member;
+
+    if (!member.joinedAt) throw new Error("Can not find joined date");
 
     const embed = new EmbedBuilder()
       .setAuthor({
         name: "Member Left",
         iconURL: member.displayAvatarURL(),
       })
-
-      .setDescription(`${member.user} - (${member.user.tag})`)
       .addFields([
         {
-          name: "Account Age",
-          value: `${member.user.createdAt}`,
+          name: "User",
+          value: `${member.user} (${member.user.tag})`,
+          inline: true,
+        },
+        {
+          name: "Member since",
+          value: `${capitalizeFirstLetter(
+            formatDistanceToNow(member.joinedAt, { addSuffix: true })
+          )}`,
+          inline: true,
+        },
+        {
+          name: "Created",
+          value: `${capitalizeFirstLetter(
+            formatDistanceToNow(member.user.createdAt, { addSuffix: true })
+          )}`,
+          inline: true,
         },
       ]);
 
