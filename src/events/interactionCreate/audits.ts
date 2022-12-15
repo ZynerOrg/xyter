7c8e6f3a6954448850302f9966a8f67301c5e91d
Vermium Sifell <vermium@zyner.org>
2022-12-15 11:36:55 +0100

    refactor(audits): :recycle: refactor interactionCreate audit log
    Refactored interactionCreate audit log file

--- src/events/interactionCreate/audits.ts
+++ src/events/interactionCreate/audits.ts
@@ -1,61 +1,40 @@
-import { BaseInteraction, ChannelType, EmbedBuilder } from "discord.js";
-import prisma from "../../handlers/database";
-import getEmbedConfig from "../../helpers/getEmbedData";
-import logger from "../../middlewares/logger";
+import { BaseInteraction, EmbedBuilder } from "discord.js";
+import auditLogger from "../../helpers/auditLogger";
 
 export default {
   execute: async (interaction: BaseInteraction) => {
-    if (interaction === null) return;
+    const { guild, user } = interaction;
+    if (!guild) throw new Error("Guild unavailable");
+    if (!user) throw new Error("User unavailable");
 
-    if (interaction.guild === null) return;
-
-    const getGuild = await prisma.guild.findUnique({
-      where: { id: interaction.guild.id },
-    });
-    if (!getGuild) throw new Error("Guild not found");
-
-    const { footerText, footerIcon, successColor } = await getEmbedConfig(
-      interaction.guild
-    );
-
-    const { client } = interaction;
-
-    if (getGuild.auditsEnabled !== true) return;
-    if (!getGuild.auditsChannelId) return;
-
-    const channel = client.channels.cache.get(`${getGuild.auditsChannelId}`);
-
-    if (!channel) return;
-    if (channel.type !== ChannelType.GuildText) return;
-
-    channel
-      .send({
-        embeds: [
-          new EmbedBuilder()
-            .setColor(successColor)
-            .setDescription(
-              `
-            **Interaction created by** ${interaction.user.username} **in** ${interaction.channel}
-            ㅤ**Interaction ID**: ${interaction.id}
-            ㅤ**Type**: ${interaction.type}
-            ㅤ**User ID**: ${interaction.user.id}
-            `
-            )
-            .setThumbnail(interaction.user.displayAvatarURL())
-            .setTimestamp()
-            .setFooter({
-              text: footerText,
-              iconURL: footerIcon,
-            }),
-        ],
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: "Interaction Created",
+        iconURL: user.displayAvatarURL(),
       })
-      .then(() => {
-        logger.debug(
-          `Audit log sent for event interactionCreate in guild ${interaction?.guild?.name} (${interaction?.guild?.id})`
-        );
-      })
-      .catch(() => {
-        logger.silly("Failed to send audit log for event interactionCreate");
-      });
+      .addFields([
+        {
+          name: "ID",
+          value: `${interaction.id}`,
+          inline: true,
+        },
+        {
+          name: "Type",
+          value: `${interaction.type}`,
+          inline: true,
+        },
+        {
+          name: "User ID",
+          value: `${interaction.user} (${interaction.user.tag})`,
+          inline: true,
+        },
+        {
+          name: "Channel",
+          value: `${interaction.channel}`,
+          inline: true,
+        },
+      ]);
+
+    await auditLogger(guild, embed);
   },
 };
