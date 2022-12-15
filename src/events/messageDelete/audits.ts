1cfd1e09e0662c4735eaba33786a21d4cf4bac21
Vermium Sifell <vermium@zyner.org>
2022-12-15 11:12:29 +0100

    refactor(audits): :recycle: add auditLog helper
    Added auditLogger function as an helper to reduce duplication of code

--- src/events/messageDelete/audits.ts
+++ src/events/messageDelete/audits.ts
@@ -1,66 +1,23 @@
-import { ChannelType, EmbedBuilder, Message } from "discord.js";
-import prisma from "../../handlers/database";
-import getEmbedConfig from "../../helpers/getEmbedData";
-import logger from "../../middlewares/logger";
+import { EmbedBuilder, Message } from "discord.js";
+import auditLogger from "../../helpers/auditLogger";
 
 export default {
   execute: async (message: Message) => {
-    if (message === null) return;
+    const { guild } = message;
+    if (!guild) throw new Error("Guild unavailable");
 
-    if (message.guild === null) return;
-
-    const { footerText, footerIcon, successColor } = await getEmbedConfig(
-      message.guild
-    );
-
-    const getGuild = await prisma.guild.findUnique({
-      where: { id: message.guild.id },
-    });
-    if (!getGuild) throw new Error("Guild not found");
-
-    const { client } = message;
-
-    if (!getGuild) throw new Error("Guild not found");
-
-    if (getGuild.auditsEnabled !== true) return;
-    if (!getGuild.auditsChannelId) return;
-
-    const channel = client.channels.cache.get(`${getGuild.auditsChannelId}`);
-
-    if (!channel) return;
-    if (channel.type !== ChannelType.GuildText) return;
-
-    channel
-      .send({
-        embeds: [
-          new EmbedBuilder()
-            .setColor(successColor)
-            .setAuthor({
-              name: message.author.username,
-              iconURL: message.author.displayAvatarURL(),
-            })
-            .setDescription(
-              `
-            **Message sent by** ${message.author} **deleted in** ${message.channel}
-            ${message.content}
-            `
-            )
-            .setTimestamp()
-            .setFooter({
-              text: footerText,
-              iconURL: footerIcon,
-            }),
-        ],
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: message.author.username,
+        iconURL: message.author.displayAvatarURL(),
       })
-      .then(() => {
-        logger.info(
-          `Audit log sent for event messageDelete in guild ${message?.guild?.name} (${message?.guild?.id})`
-        );
-      })
-      .catch(() => {
-        throw new Error(
-          `Audit log failed to send for event messageDelete in guild ${message?.guild?.name} (${message?.guild?.id})`
-        );
-      });
+      .setDescription(
+        `
+    **Message sent by** ${message.author} **deleted in** ${message.channel}
+    ${message.content}
+    `
+      );
+
+    await auditLogger(guild, embed);
   },
 };
