d62baa0b9c70094cade6baddef76dd35d96eb45e
Vermium Sifell <vermium@zyner.org>
2022-12-21 07:46:42 +0100

    refactor: :technologist: refactored event messageCreate
    Remove unnecessary code in the messageCreate event

--- src/events/messageCreate/index.ts
+++ src/events/messageCreate/index.ts
@@ -1,22 +1,15 @@
 import { Message } from "discord.js";
 import { IEventOptions } from "../../interfaces/EventOptions";
-import modules from "./modules";
+import countersExecute from "./modules/counters";
+import creditsExecute from "./modules/credits";
+import pointsExecute from "./modules/points";
 
 export const options: IEventOptions = {
   type: "on",
 };
 
-// Execute the function
 export const execute = async (message: Message) => {
-  await modules.credits.execute(message);
-  await modules.points.execute(message);
-  await modules.counters.execute(message);
-
-  if (!message.member) return;
-  if (message.author.bot) return;
-
-  // client.emit("guildMemberAdd", message.member);
-  // client.emit("guildMemberRemove", message.member);
-  // client.emit("messageDelete", message);
-  // client.emit("messageUpdate", message, message);
+  await creditsExecute(message);
+  await pointsExecute(message);
+  await countersExecute(message);
 };
--- src/events/messageCreate/modules/counters/index.ts
+++ src/events/messageCreate/modules/counters/index.ts
@@ -2,68 +2,66 @@ import { ChannelType, Message } from "discord.js";
 import prisma from "../../../../handlers/database";
 import logger from "../../../../middlewares/logger";
 
-export default {
-  execute: async (message: Message) => {
-    const { guild, author, content, channel } = message;
+export default async (message: Message) => {
+  const { guild, author, content, channel } = message;
 
-    if (!guild) return;
-    if (author.bot) return;
-    if (channel?.type !== ChannelType.GuildText) return;
+  if (!guild) return;
+  if (author.bot) return;
+  if (channel?.type !== ChannelType.GuildText) return;
 
-    const messages = await message.channel.messages.fetch({ limit: 2 });
-    const lastMessage = messages.last();
+  const messages = await message.channel.messages.fetch({ limit: 2 });
+  const lastMessage = messages.last();
 
-    const channelCounter = await prisma.guildCounters.findUnique({
-      where: {
-        guildId_channelId: {
-          guildId: guild.id,
-          channelId: channel.id,
-        },
+  const channelCounter = await prisma.guildCounters.findUnique({
+    where: {
+      guildId_channelId: {
+        guildId: guild.id,
+        channelId: channel.id,
       },
-    });
+    },
+  });
 
-    if (!channelCounter) {
-      logger.debug("No counters found in channel.");
-      return;
-    }
+  if (!channelCounter) {
+    logger.debug("No counters found in channel.");
+    return;
+  }
 
-    if (
-      lastMessage?.author.id === author.id &&
-      channel.id === channelCounter.channelId
-    ) {
-      logger.silly(
-        `${author.username} sent the last message therefor not allowing again.`
-      );
-      await message.delete();
-      return;
-    }
+  if (
+    lastMessage?.author.id === author.id &&
+    channel.id === channelCounter.channelId
+  ) {
+    logger.silly(
+      `${author.username} sent the last message therefor not allowing again.`
+    );
+    await message.delete();
+    return;
+  }
 
-    if (content !== channelCounter.triggerWord) {
-      logger.silly(
-        `Counter word ${channelCounter.triggerWord} does not match message ${content}`
-      );
+  if (content !== channelCounter.triggerWord) {
+    logger.silly(
+      `Counter word ${channelCounter.triggerWord} does not match message ${content}`
+    );
 
-      await message.delete();
-      return;
-    }
+    await message.delete();
+    return;
+  }
 
-    const updateGuildCounter = await prisma.guildCounters.update({
-      where: {
-        guildId_channelId: {
-          guildId: guild.id,
-          channelId: channel.id,
-        },
+  const updateGuildCounter = await prisma.guildCounters.update({
+    where: {
+      guildId_channelId: {
+        guildId: guild.id,
+        channelId: channel.id,
       },
-      data: {
-        count: {
-          increment: 1,
-        },
+    },
+    data: {
+      count: {
+        increment: 1,
       },
-    });
+    },
+  });
 
-    logger.silly(updateGuildCounter);
+  logger.silly(updateGuildCounter);
 
-    if (!updateGuildCounter)
-      logger.error(`Failed to update counter - ${updateGuildCounter}`);
-  },
+  if (!updateGuildCounter)
+    logger.error(`Failed to update counter - ${updateGuildCounter}`);
 };
