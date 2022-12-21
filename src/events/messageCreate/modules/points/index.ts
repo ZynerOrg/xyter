d35242f33ac77311e40db1f50fd370af22b80f10
Vermium Sifell <vermium@zyner.org>
2022-12-21 07:46:42 +0100

    refactor: :technologist: refactored event messageCreate
    Remove unnecessary code in the messageCreate event

--- src/events/messageCreate/modules/points/index.ts
+++ src/events/messageCreate/modules/points/index.ts
@@ -3,65 +3,63 @@ import prisma from "../../../../handlers/database";
 import cooldown from "../../../../middlewares/cooldown";
 import logger from "../../../../middlewares/logger";
 
-export default {
-  execute: async (message: Message) => {
-    const { guild, author, content, channel } = message;
+export default async (message: Message) => {
+  const { guild, author, content, channel } = message;
 
-    if (!guild) return;
-    if (author.bot) return;
-    if (channel.type !== ChannelType.GuildText) return;
+  if (!guild) return;
+  if (author.bot) return;
+  if (channel.type !== ChannelType.GuildText) return;
 
-    const upsertGuildConfigPoints = await prisma.guildConfigPoints.upsert({
-      where: {
-        id: guild.id,
-      },
-      update: {},
-      create: {
-        guild: {
-          connectOrCreate: {
-            create: {
-              id: guild.id,
-            },
-            where: {
-              id: guild.id,
-            },
+  const upsertGuildConfigPoints = await prisma.guildConfigPoints.upsert({
+    where: {
+      id: guild.id,
+    },
+    update: {},
+    create: {
+      guild: {
+        connectOrCreate: {
+          create: {
+            id: guild.id,
+          },
+          where: {
+            id: guild.id,
           },
         },
       },
-      include: {
-        guild: true,
+    },
+    include: {
+      guild: true,
+    },
+  });
+
+  logger.silly(upsertGuildConfigPoints);
+
+  if (content.length < upsertGuildConfigPoints.minimumLength) return;
+
+  await cooldown(
+    guild,
+    author,
+    "event-messageCreate-points",
+    upsertGuildConfigPoints.timeout,
+    true
+  );
+
+  const updateGuildMember = await prisma.guildMember.update({
+    where: {
+      userId_guildId: {
+        userId: author.id,
+        guildId: guild.id,
       },
-    });
-
-    logger.silly(upsertGuildConfigPoints);
-
-    if (content.length < upsertGuildConfigPoints.minimumLength) return;
-
-    await cooldown(
-      guild,
-      author,
-      "event-messageCreate-points",
-      upsertGuildConfigPoints.timeout,
-      true
-    );
-
-    const updateGuildMember = await prisma.guildMember.update({
-      where: {
-        userId_guildId: {
-          userId: author.id,
-          guildId: guild.id,
-        },
+    },
+    data: {
+      pointsEarned: {
+        increment: upsertGuildConfigPoints.rate,
       },
-      data: {
-        pointsEarned: {
-          increment: upsertGuildConfigPoints.rate,
-        },
-      },
-    });
+    },
+  });
 
-    logger.silly(updateGuildMember);
+  logger.silly(updateGuildMember);
 
-    if (!updateGuildMember)
-      throw new Error("Failed to update guildMember object");
-  },
+  if (!updateGuildMember)
+    throw new Error("Failed to update guildMember object");
 };
