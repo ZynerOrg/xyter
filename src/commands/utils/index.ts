c19a1d865314132a288467ca9841b7106d2e5149
Vermium Sifell <vermium@zyner.org>
2023-01-26 14:45:35 +0100

    Improve structure subcommands utils

--- src/commands/utils/index.ts
+++ src/commands/utils/index.ts
@@ -1,31 +1,20 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
-// Modules
-import {
-  builder as AboutBuilder,
-  execute as AboutExecute,
-} from "./modules/about";
-import {
-  builder as AvatarBuilder,
-  execute as AvatarExecute,
-} from "./modules/avatar";
+import * as about from "./subcommands/about";
+import * as avatar from "./subcommands/avatar";
 
 export const builder = new SlashCommandBuilder()
   .setName("utils")
   .setDescription("Common utility.")
+  .addSubcommand(about.builder)
+  .addSubcommand(avatar.builder);
 
-  // Modules
-  .addSubcommand(AboutBuilder)
-  .addSubcommand(AvatarBuilder);
-
-// Execute the command
 export const execute = async (interaction: ChatInputCommandInteraction) => {
   switch (interaction.options.getSubcommand()) {
     case "about":
-      await AboutExecute(interaction);
+      await about.execute(interaction);
       break;
     case "avatar":
-      await AvatarExecute(interaction);
+      await avatar.execute(interaction);
       break;
     default:
       throw new Error(
