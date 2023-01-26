2c35cf7885b0bfc67c3c6875461264cbecb52ced
Vermium Sifell <vermium@zyner.org>
2023-01-26 16:57:50 +0100

    Refactor and improve UX

--- src/commands/reputation/index.ts
+++ src/commands/reputation/index.ts
@@ -1,34 +1,25 @@
-// Dependencies
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import * as check from "./subcommands/check";
+import * as repute from "./subcommands/repute";
 
-// Modules
-import {
-  builder as CheckBuilder,
-  execute as CheckExecute,
-} from "./subcommands/check";
-import {
-  builder as ReputeBuilder,
-  execute as ReputeExecute,
-} from "./subcommands/repute";
-
-// Function
 export const builder = new SlashCommandBuilder()
   .setName("reputation")
-  .setDescription("See and repute users to show other how trustworthy they are")
+  .setDescription(
+    "See and give reputation to users to show others how trustworthy they are"
+  )
   .setDMPermission(false)
+  .addSubcommand(repute.builder)
+  .addSubcommand(check.builder);
 
-  // Modules
-  .addSubcommand(ReputeBuilder)
-  .addSubcommand(CheckBuilder);
-
-// Execute function
 export const execute = async (interaction: ChatInputCommandInteraction) => {
-  if (interaction.options.getSubcommand() === "repute") {
-    await ReputeExecute(interaction);
-    return;
-  }
-  if (interaction.options.getSubcommand() === "check") {
-    await CheckExecute(interaction);
-    return;
+  switch (interaction.options.getSubcommand()) {
+    case "repute":
+      await repute.execute(interaction);
+      break;
+    case "check":
+      await check.execute(interaction);
+      break;
+    default:
+      break;
   }
 };
