83c0af64c9bca330f73de97580dd3020271174c8
Vermium Sifell <vermium@zyner.org>
2023-10-01 17:15:38 +0200

    refactor: :recycle: improve checkPermission function
    Made use of optional chaining (?.) to get rid of a if statement

--- src/utils/checkPermission.ts
+++ src/utils/checkPermission.ts
@@ -1,9 +1,6 @@
 import { Interaction, PermissionResolvable } from "discord.js";
 
 export default (interaction: Interaction, permission: PermissionResolvable) => {
-  if (!interaction.memberPermissions)
-    throw new Error("Failed to check your permissions");
-
-  if (!interaction.memberPermissions.has(permission))
+  if (!interaction.memberPermissions?.has(permission))
     throw new Error(`You do not have the required permission: ${permission}`);
 };
